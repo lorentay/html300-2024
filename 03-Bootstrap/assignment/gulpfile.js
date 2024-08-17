5d05572bed4da8c0bded825cd43e178199969365
Susanlt <susanlt@me.com>
2024-08-16 20:50:38 -0700

    added landing page draft content structure

--- 03-Bootstrap/assignment/gulpfile.js
+++ 03-Bootstrap/assignment/gulpfile.js
@@ -25,7 +25,7 @@ task('sass', function() {
     return src(paths.scss.src)
         .pipe(sass({
             includePaths: [paths.scss],
-            outputStyle: 'compressed'
+            outputStyle: 'expanded'
         }).on('error', function(err) {
             console.log(err.message);
             this.emit('end');
